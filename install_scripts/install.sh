f7c47b261e96e6ee6e26deeac6fdbf62b1b7b36a
Alison Williams <allylavanda@aol.com>
2022-02-04 15:09:14 -0500

    Create install.sh

---
+++ install_scripts/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Copyright (c) 2022 Sjaeledyr
+
+# Hostname of the installed machine.
+HOSTNAME='caik'
+
+# Root password (leave blank to be prompted).
+ROOT_PASSWORD='a'
+
+# USER
+USER_NAME='user'
+
+# PASS
+USER_PASSWORD='a'
+
+# System timezone.
+TIMEZONE='America/New_York'
+
+# Have /tmp on a tmpfs or not.  Leave blank to disable.
+# Only leave this blank on systems with very little RAM.
+TMP_ON_TMPFS='TRUE'
+
+KEYMAP='us'
+
+# Choose your video driver
+# For Intel
+#VIDEO_DRIVER="i915"
+# For nVidia
+VIDEO_DRIVER="nouveau"
+# For ATI
+#VIDEO_DRIVER="radeon"
+# For generic stuff
+#VIDEO_DRIVER="vesa"
+
+# Wireless device, leave blank to not use wireless and use DHCP instead.
+WIRELESS_DEVICE="wlan0"
+# For tc4200's
+#WIRELESS_DEVICE="eth1"
+
+install() {
+  echo 'Starting Caik-Linux install script.'
+  
+  echo 'Partitioning Drive.'
+  part_drive "$DRIVE"
